ead2ee3736eddfed9b855a723c9d3b44e8a017c1
Will Rathlou <willrathlou@hotmail.com>
2018-02-04 15:04:04 +0000

    stringbuilder

---
+++ StringBuilderDemo/stringbuilder test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringbuilder_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Values.Value val = new Values.Value();
+            Console.WriteLine("Enter a word or phrase and i'll attempt to replicate it");
+            val.GetInput = Console.ReadLine();
+            val.GetOutput = Constructor(val.GetInput);
+            Console.WriteLine(val.GetOutput);
+            Console.ReadLine();
+        }
+        static string Constructor(string bridge)
+        {
+            StringBuilder str = new StringBuilder();
+            int count = 0;
+            do
+            {
+                str.Append(bridge[count] + " ");
+                count++;
+            } while (bridge.Length > count);
+            bridge = str.ToString();
+            return bridge;
+        }
+    }
+    
+}
+namespace Values
+{
+    public class Value
+    {
+        private string Input;
+        private string Output;
+
+        public string GetInput
+        {
+            get { return Input; }
+            set { Input = value; }
+        }
+        public string GetOutput
+        {
+            get { return Output; }
+            set { Output = value; }
+        }
+    }
+}
